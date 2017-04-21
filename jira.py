d9e7137c4b9c1e3bfc8fc7cbad1c8277963e0ab4
Mikhail Saburenkov <allo.superman@gmail.com>
2017-04-21 11:20:53 +0300

    Add urls to stuck issues message and change title

--- jira.py
+++ jira.py
@@ -4,7 +4,7 @@ import config
 
 def strip_issue(issue):
     return {
-        'key': issue['key'],
+        'url': '{}/browse/{}'.format(config.jira_url, issue['key']),
         'summary': issue['fields']['summary'],
         'assignee': issue['fields']['assignee']['name'],
         'status': issue['fields']['status']['name'],
@@ -12,8 +12,8 @@ def strip_issue(issue):
 
 
 def string_template(item):
-    return '{key} {summary} @{assignee} `{status}`'.format(
-        key=item['key'],
+    return '{url} {summary} @{assignee} `{status}`'.format(
+        url=item['url'],
         summary=item['summary'],
         assignee=config.jira_slack_mapping.get(item['assignee'], item['assignee']).lower(),
         status=item['status']
@@ -38,5 +38,5 @@ def get_issues():
     if not result:
         return None
     result = (strip_issue(x) for x in result)
-    result = 'Stuck issues:\n' + '\n'.join([string_template(x) for x in result])
+    result = '*Stuck issues*\n' + '\n'.join([string_template(x) for x in result])
     return result
